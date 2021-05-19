37789c75a6aea81ead85a4a4291b3a601af014f8
VojtechStransky <stranskyvo@gmail.com>
2021-05-20 00:34:58 +0200

    Icons added

--- Pokerweb/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/Chat.js
+++ Pokerweb/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/Chat.js
@@ -9,6 +9,8 @@ function Hide() {
     }
 }
 
+var loaded = false;
+
 window.onload = function () {
 
     var isPlaying = "false";
@@ -17,14 +19,19 @@ window.onload = function () {
 
     connection.on("ShowPlayButton", function () {
         document.getElementById("PlayButton").style.display = "inline";
+        document.getElementById("waitingText").style.display = "none";
     });
 
     connection.on("ReceiveMessage", function () {
         $(function () {
-            $('#grid').load('/GamePage/PlayersPartial?key=' +
+            var page = '/GamePage/PlayersPartial?key=' +
                 document.getElementById("key").innerHTML +
-                "&name=" + 
-                document.getElementById("name").innerHTML)
+                "&name=" +
+                document.getElementById("name").innerHTML;
+
+            $('#grid').load(page, function () {
+                add();
+            })
         })
     });
 
@@ -34,7 +41,6 @@ window.onload = function () {
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "inline";
         }
-        isPlaying = "true";
     });
 
     connection.start().then(function () {
@@ -93,5 +99,93 @@ window.onload = function () {
         connection.invoke("LeaveMessage", document.getElementById("key").innerHTML,
             document.getElementById("name").innerHTML, isPlaying)
     };
- 
+
+    function Next() {
+
+        var x = document.getElementsByClassName("pContainer");
+        var a = document.getElementById("actual");
+        var number = 0;
+
+        var z;
+
+        if (actualCard == null) {
+            number = a.innerHTML + 1;
+        } else {
+            number = actualCard + 1;
+        }
+
+        for (z = 0; z < x.length; z++) {
+            x[z].style.display = "none"
+        }
+
+        if (number >= x.length) {
+            number = 0;
+        }
+
+        if ((number <= x.length) && (number >= 0) && (x[number] != null)) {
+            x[number].style.display = "block";
+            container.classList.remove("slide");
+            container.classList.remove("slideReversed");
+            void container.offsetWidth;
+            container.classList.add("slide");
+        }
+
+        actualCard = number;
+        
+    }
+
+    function Previous() {
+        var container = document.getElementById("container");
+        var x = document.getElementsByClassName("pContainer");
+        var a = document.getElementById("actual");
+        var number = 0;
+
+        var z;
+
+        if (actualCard == null) {
+            number = a.innerHTML - 1;
+        } else {
+            number = actualCard - 1;
+        }
+
+        for (z = 0; z < x.length; z++) {
+            x[z].style.display = "none"
+        }
+
+        if (number < 0) {
+            number = x.length - 1;
+        }
+
+        if ((number <= x.length) && (number >= 0) && (x[number] != null)) {
+            x[number].style.display = "block";
+            container.classList.remove("slideReversed");
+            container.classList.remove("slide");
+            void container.offsetWidth;
+            container.classList.add("slideReversed");
+        }
+
+        actualCard = number;
+    }
+
+    var actualCard = null;
+    var x = document.getElementById("nextCardBtn");
+    var y = document.getElementById("previousCardBtn");
+
+    function add() {
+        y.onclick = function () { Previous() };
+        x.onclick = function () { Next() };
+
+        var inGame = document.getElementById("inGame").innerHTML;
+
+        console.log(inGame);
+
+        if (inGame == "True") {
+            document.getElementById("nextCardBtn").style.display = "block";
+            document.getElementById("previousCardBtn").style.display = "block";
+            document.getElementById("waitingText").style.display = "none";
+        } else {
+            document.getElementById("nextCardBtn").style.display = "none";
+            document.getElementById("previousCardBtn").style.display = "none";
+        }
+    }
 };
